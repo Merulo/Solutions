af315badcdc9ed34eb8f0a94661c6073996ff18c
Merulo <damian.p.nowak@gmail.com>
2018-09-17 20:40:57 +0200

    removing commented code

--- minimumNumberFromIDSequence/src/Solution.hpp
+++ minimumNumberFromIDSequence/src/Solution.hpp
@@ -24,19 +24,6 @@ std::string convert(const std::string& input)
     std::stack<char> possibleCharacters;
     char starting = '1';
 
-    // for(int i = 0; i <= input.length(); i++)
-    // {
-    //     possibleCharacters.push(starting);
-    //     starting++;
-    //     if(i == input.length() || input[i] == 'I')
-    //     {
-    //         std::cout<<"starting "<< starting<<std::endl;
-    //         std::cout<<"result "<< result<<std::endl;
-
-    //         addToResult(result, possibleCharacters);
-    //     }
-    // }
-
     for (const auto& character : input)
     {
         possibleCharacters.push(starting);
@@ -46,6 +33,7 @@ std::string convert(const std::string& input)
             addToResult(result, possibleCharacters);
         }
     }
+    
     possibleCharacters.push(starting);
     addToResult(result, possibleCharacters);
     return result;
